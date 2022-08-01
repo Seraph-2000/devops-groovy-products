0817e0c723304242419d4dcb941838471cf215e8
Seraph-2000 <santkunal2000@gmail.com>
2022-08-01 11:34:06 +0530

    1st cicd proj

---
+++ src/main/groovy/com/myapp/spring/DevopsGroovyProductsApplication.groovy
@@ -0,0 +1,19 @@
+package com.myapp.spring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+
+@SpringBootApplication
+class DevopsGroovyProductsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DevopsGroovyProductsApplication, args)
+	}
+	
+	@GetMapping
+	def sayHello() {
+		return "Hello"
+	}
+
+}
