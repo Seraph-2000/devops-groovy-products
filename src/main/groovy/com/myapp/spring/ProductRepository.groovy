077742b9d45f2b29bc25a8ecfe55d3fce94839c4
Seraph-2000 <santkunal2000@gmail.com>
2022-08-03 16:57:06 +0530

    test case added

---
+++ src/main/groovy/com/myapp/spring/ProductRepository.groovy
@@ -0,0 +1,9 @@
+package com.myapp.spring
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProductRepository extends JpaRepository<Product,Integer> {
+	
+}
