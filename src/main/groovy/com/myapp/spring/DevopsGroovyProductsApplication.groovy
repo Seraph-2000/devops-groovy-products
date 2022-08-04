7418cc775d39ca915341791692662989987935ac
Seraph-2000 <santkunal2000@gmail.com>
2022-08-04 12:43:29 +0530

    'updated3'

--- src/main/groovy/com/myapp/spring/DevopsGroovyProductsApplication.groovy
+++ src/main/groovy/com/myapp/spring/DevopsGroovyProductsApplication.groovy
@@ -3,9 +3,10 @@ package com.myapp.spring
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class DevopsGroovyProductsApplication {
+class DevopsGroovyProductsApplication extends SpringBootServletInitializer{
 
 	static void main(String[] args) {
 		SpringApplication.run(DevopsGroovyProductsApplication, args)
